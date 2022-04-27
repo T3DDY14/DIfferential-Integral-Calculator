b68c8c5677198ef94441b8abf0a8c2f606b2f218
T3DDY14 <41326850+T3DDY14@users.noreply.github.com>
2022-04-27 23:57:44 +0100

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -1,8 +1,15 @@
-from sympy import *
-from sympy import *
-x, y, z = symbols('x y z')
-init_printing(use_unicode=True)
-print(diff(cos(x), x))
-print(diff(exp(x**2), x))
-eq = input("equation")
-print(diff(exp(eq),x))
+x = "cos","cosec"
+xinit = 0
+print(x)
+while xinit < 3:
+    if x[xinit] == "cos":
+        print(x[xinit])
+        print("cos")
+        xinit = xinit + 1
+    elif x[xinit] == "cosec":
+        print(x[xinit])
+        print("cosec")
+        xinit = xinit + 1    
+    else:
+        print("done")
+
